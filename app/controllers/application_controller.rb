f4c69dd87462cb784e17d57d61c8c71b1bb4661a
Stephanie Sison <sjsison27@icloud.com>
2022-12-30 09:41:43 -0800

    commit tree aaaea6653da3ec96822a8cc9292240d1d7f0cd69

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,36 @@
+require 'pry'
+
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
+
+  delete '/reviews/:id' do
+    # find the review using the ID
+    review = Review.find(params[:id])
+    # delete the review 
+    review.destroy
+    # send a response with the delted review as JSON
+    review.to_json
+  end
+
+  post '/reviews' do
+    review = Review.create(
+      score: params[:score],
+      comment: params[:comment],
+      game_id: params[:game_id],
+      user_id: params[:user_id]
+    )
+    review.to_json
+  end
+
+  patch '/reviews/:id' do
+    review = Review.find(params[:id])
+    review.update(
+      score: params[:score],
+      comment: params[:comment]
+    )
+    review.to_json
+  end
+  # binding.pry
 
   get '/games' do
     games = Game.all.order(:title).limit(10)
@@ -15,5 +46,7 @@ class ApplicationController < Sinatra::Base
       } }
     })
   end
+
+
 
 end
